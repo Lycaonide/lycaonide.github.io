1cdfcb25ff4b76aa2b113f7d26f63c5859dc1409
Lycaonide <1306273519@qq.com>
2026-09-12 09:18:21 +0800

    fix: 气泡去竖排writingMode改横排、离人物更远-90、问候语小助手口吻、双击拦截库气泡只锁按钮栏、拖动期间定时保持按钮栏

--- src/config/pioConfig.ts
+++ src/config/pioConfig.ts
@@ -180,7 +180,7 @@ export const live2dWidgetConfig: Live2DWidgetConfig = {
 		// 气泡开关
 		enable: true,
 		// 初始欢迎消息
-		welcomeMessage: ["你好呀！欢迎来到我的小站，一起交流学习～"],
+		welcomeMessage: ["你好呀！我是小助手，有什么可以帮你？"],
 		// 循环提示内容
 		messages: [
 			"有什么需要帮助的吗？",
